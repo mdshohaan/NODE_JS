47f459c22b879c5de902f7e92c864685757ed227
mdshohaan <irfanulislamdu@gmail.com>
2025-03-09 10:35:51 +0600

    Add module

--- core_module/index2.js
+++ core_module/index2.js
@@ -3,5 +3,11 @@ const fs = require("fs");
 fs.writeFileSync("myFile.txt", "hello world");
 fs.appendFileSync("myFile.txt", "how are you");
 
+// Synchoronous way fs
 const data = fs.readFileSync("myFile.txt");
-console.log(data);
+// console.log(data.toString());
+
+// Asynchoronous way fs
+fs.readFile("myFile.txt", (err, data) => {
+  console.log(data.toString());
+});
