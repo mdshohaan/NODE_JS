45bd3613c02cf0df91acca89eb5bf7176556ba65
mdshohaan <irfanulislamdu@gmail.com>
2025-03-09 07:00:11 +0600

    first commit

---
+++ tutorial/people.js
@@ -0,0 +1,11 @@
+//  Module Basic also IFII Function and By Default Return
+// (function (exports, require, module, __dirname, __filename) {
+const people = ["sakib", "tamim"];
+const a = 6;
+module.exports = {
+  people,
+  a,
+};
+//  console.log(module);
+//  return
+// });
