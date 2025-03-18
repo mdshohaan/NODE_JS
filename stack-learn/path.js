949ff6e37a7bd2cfea125323c1eebd1461eaae71
mdshohaan <irfanulislamdu@gmail.com>
2025-03-18 15:27:03 +0600

    Chore:Cteate all fs

--- stack-learn/path.js
+++ stack-learn/path.js
@@ -1,7 +1,7 @@
 const path = require("path");
-// console.log(path.basename(__filename));
-// console.log(path.basename(__dirname));
-// console.log(path.extname(__filename));
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+console.log(path.extname(__filename));
 
 // manually path create
 const pathObj = {
@@ -9,6 +9,19 @@ const pathObj = {
   name: "test",
   ext: "js",
 };
-// console.log(path.format(pathObj));
+console.log(path.format(pathObj)); // user/local/test.js
 
-// console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute(__filename)); // true
+
+console.log(path.join("user", "local", "mainFile", "test.js")); // user/local/mainFile/test.js
+console.log(path.resolve(__dirname, "script", "test.js"));
+// /Users/mohammadirfanulislam/Desktop/nodeJS/stack-learn/script/test.js
+
+console.log(path.parse(__filename));
+// {
+//   root: '/',
+//   dir: '/Users/mohammadirfanulislam/Desktop/nodeJS/stack-learn',
+//   base: 'path.js',
+//   ext: '.js',
+//   name: 'path'
+// }
