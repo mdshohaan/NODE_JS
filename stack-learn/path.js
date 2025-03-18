ef424efbee0ca78b931042f6be0fcfc0f0fd3c31
mdshohaan <irfanulislamdu@gmail.com>
2025-03-18 15:10:51 +0600

    Chore:Add fs system nodeJS

---
+++ stack-learn/path.js
@@ -0,0 +1,14 @@
+const path = require("path");
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
+// console.log(path.extname(__filename));
+
+// manually path create
+const pathObj = {
+  dir: "user/local",
+  name: "test",
+  ext: "js",
+};
+// console.log(path.format(pathObj));
+
+// console.log(path.isAbsolute(__filename));
