92750baf57d10875e0dd27d855ceacc361c45d6d
mdshohaan <irfanulislamdu@gmail.com>
2025-03-19 17:30:07 +0600

    Chore:Add res and req

--- stack-learn/intro-to-express/app.js
+++ stack-learn/intro-to-express/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("I am listening");
+});
+
+app.get("*", (req, res) => {
+  res.send("I am Not Found");
+});
 
 app.listen(PORT, () => {
   console.log("Server is Running");
