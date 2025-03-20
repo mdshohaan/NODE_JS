24a1696531c4c7ff01273a09fe314394baad6cc2
mdshohaan <irfanulislamdu@gmail.com>
2025-03-21 00:13:56 +0600

    Chore:Add Contacts class

--- stack-learn/intro-to-project/contacts-app/Contacts.js
+++ stack-learn/intro-to-project/contacts-app/Contacts.js
@@ -1,4 +1,4 @@
-class Contact {
+class Contacts {
   constructor() {
     this.contacts = [];
   }
@@ -8,4 +8,26 @@ class Contact {
   getContactsById(id) {
     return this.contacts.find((contact) => contact.id === id);
   }
+  createContact(contact) {
+    contact.id = this.contacts.length + 1;
+    this.contacts.push(contact);
+    return contact;
+  }
+  updateContactById(id, updatedContact) {
+    let index = this.contacts.findIndex((contact) => contact.id === id);
+    this.contacts[index].name =
+      updatedContact.name || this.contacts[index].name;
+    this.contacts[index].phone =
+      updatedContact.phone || this.contacts[index].phone;
+    this.contacts[index].email =
+      updatedContact.email || this.contacts[index].email;
+    return this.contacts[index];
+  }
+  deleteContactById(id) {
+    let index = this.contacts.findIndex((contact) => contact.id === id);
+    let deletedObj = this.contacts[index];
+    this.contacts = this.contacts.filter((contact) => contact.id !== id);
+    return deletedObj;
+  }
 }
+exports.contacts = new Contacts();
