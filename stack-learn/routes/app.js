8331dbd55a247e1b1ffad21ac4bfc63325bf48d7
mdshohaan <irfanulislamdu@gmail.com>
2025-03-20 08:51:29 +0600

    Chore:Refactor the code

--- stack-learn/routes/app.js
+++ stack-learn/routes/app.js
@@ -4,17 +4,26 @@ const app = express();
 const userRouter = require("./userRoute");
 const postRoute = require("./postRoute");
 
+// middleware
 app.use(morgan("dev"));
 
 // User Router
 app.use("/user", userRouter);
-// Post Rouute
+// Post Router
 app.use("/posts", postRoute);
 
+// Dynamic Router using Parameter
+app.get("/products/:prodId/reviews/:revId", (req, res) => {
+  console.log(req.params);
+  res.send(req.params);
+});
+
+// Default Route
 app.get("/", (req, res) => {
   res.send("I am listening");
 });
 
+// Port Listeninng
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is Running");
