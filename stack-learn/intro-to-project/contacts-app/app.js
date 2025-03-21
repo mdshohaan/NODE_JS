fc5433f4c0cade13884ada05cf957a3d7bab28e6
mdshohaan <irfanulislamdu@gmail.com>
2025-03-21 10:09:51 +0600

    Chore:define all routes

--- stack-learn/intro-to-project/contacts-app/app.js
+++ stack-learn/intro-to-project/contacts-app/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const contactRoute = require("./contactRoute");
+const app = express();
 
 // middleware
 app.use(morgan("dev"));
+
+// Contact Routes
+app.use("/contact", contactRoute);
+
+app.get("*", (req, res) => {
+  res.send("Plse use the Correct Routes");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
