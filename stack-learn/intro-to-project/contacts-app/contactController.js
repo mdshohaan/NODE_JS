3d63b5af458a5ad0cce6bb5ca6a282d0e598ce5e
mdshohaan <irfanulislamdu@gmail.com>
2025-03-21 12:34:25 +0600

    Chore:handle all routes

--- stack-learn/intro-to-project/contacts-app/contactController.js
+++ stack-learn/intro-to-project/contacts-app/contactController.js
@@ -13,3 +13,32 @@ exports.createContact = (req, res) => {
   });
   res.json(contact);
 };
+
+exports.getContactById = (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+  let contact = contacts.getContactsById(id);
+  res.json(contact);
+};
+
+exports.updateContact = (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+  const { name, phone, email } = req.body;
+  let contact = contacts.updateContactById(id, {
+    name,
+    phone,
+    email,
+  });
+  res.json(contact);
+};
+
+exports.deleteContact = (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+  let contact = contacts.deleteContactById(id);
+  res.json(contact);
+};
+
+// 👉 req.params, req.query, এবং req.body থেকে পাওয়া ডাটা ডিফল্টভাবে string হয়।
+// 👉 কারণ HTTP request সবসময় text-based হয়, তাই যেকোনো ডাটা প্রথমে string হিসেবে আসে।
--- stack-learn/intro-to-project/contacts-app/contactRoute.js
+++ stack-learn/intro-to-project/contacts-app/contactRoute.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
-const { getAllContacts, createContact } = require("./contactController");
+const {
+  getAllContacts,
+  createContact,
+  getContactById,
+  updateContact,
+  deleteContact,
+} = require("./contactController");
 
 router.get("/", getAllContacts);
 router.post("/", createContact);
-// router.get("/:id");
-// router.put("/:id");
-// router.delete("/:id");
+router.get("/:id", getContactById);
+router.put("/:id", updateContact);
+router.delete("/:id", deleteContact);
 
 module.exports = router;
