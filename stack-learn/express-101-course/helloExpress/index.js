0f7c58ba3574295eb7925f0ed52772efc3d1ca22
mdshohaan <irfanulislamdu@gmail.com>
2025-03-26 22:51:51 +0600

    feat:fs module explore

--- stack-learn/express-101-course/helloExpress/index.js
+++ stack-learn/express-101-course/helloExpress/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello User");
+  fs.readFile("./pages/pages.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      return `<h3>I am Error</h3>`;
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 app.listen(3000, () => {
