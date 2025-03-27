c6d2810908c069a11b6bfcfcdea360b678a23649
mdshohaan <irfanulislamdu@gmail.com>
2025-03-27 17:35:54 +0600

    feat:Add mount app

--- express/index2.js
+++ express/index2.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const app = express();
 const handle = require("./handle");
+
+// middleware
+const app = express();
+const admin = express();
+
+// admin.on("mount", function (parent) {
+//   console.log("Admin Mounted");
+//   console.log(parent); // refers to the parent app
+// });
 
 app.locals.title = "Irfanul Islam";
 
-app.get("/", handle);
+app.use("/admin", admin);
+
+admin.get("/dashboard", (req, res) => {
+  console.log(admin.mountpath); // get Route path
+  res.send("Welcome to admin Dashboard");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to application");
+});
 
 app.listen(3000, () => {
   console.log("Server is Running");
