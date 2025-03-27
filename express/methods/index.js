a8f7db9d74988be59977375af20050d0260c0d91
mdshohaan <irfanulislamdu@gmail.com>
2025-03-27 20:49:27 +0600

    feat;Add all methods

--- express/methods/index.js
+++ express/methods/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.enable("case sensitive routing");
 app.disable("case sensitive routing");
 
@@ -17,6 +19,18 @@ app.all("/user/:id", (req, res) => {
   console.log(req.userDetails);
   res.send("Welcome to application");
 });
+
+app
+  .route("/about/mission")
+  .get((req, res) => {
+    res.render("./pages/index");
+  })
+  .post((req, res) => {
+    res.send("Welcome to application Post");
+  })
+  .put((req, res) => {
+    res.send("Welcome to application Put");
+  });
 
 app.listen(3000, () => {
   console.log("Server is Running");
