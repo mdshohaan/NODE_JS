93f7e67ca56058fca74fbf610e356f5d2ebe2305
mdshohaan <irfanulislamdu@gmail.com>
2025-03-28 20:11:41 +0600

    feat:response all format

--- express/response/methods/index.js
+++ express/response/methods/index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send("Hello Test");
+});
+
 app.get("/about", (req, res) => {
-  res.format({
-    "text/plain": () => {
-      res.send("hi");
-    },
-    "text/html": () => {
-      res.render("pages/about");
-    },
-    "application/json": () => {
-      res.json({
-        name: "Bangladesh",
-      });
-    },
-    default: () => {
-      res.status(403).send("Not Acceptable");
-    },
-  });
+  // res.format({
+  //   "text/plain": () => {
+  //     res.send("hi");
+  //   },
+  //   "text/html": () => {
+  //     res.render("pages/about");
+  //   },
+  //   "application/json": () => {
+  //     res.json({
+  //       name: "Bangladesh",
+  //     });
+  //   },
+  //   default: () => {
+  //     res.status(403).send("Not Acceptable");
+  //   },
+  // });
+  // res
+  //   .cookie("irfan", {
+  //     domain: "example.com",
+  //     secure: true,
+  //   })
+  //   .end();
+  // res.redirect("/test");
+  // res.end();
 });
 
 app.listen(3000, () => {
