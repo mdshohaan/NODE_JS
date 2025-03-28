a046db89ca9836332246432b2af3dcf234edd423
mdshohaan <irfanulislamdu@gmail.com>
2025-03-29 00:19:38 +0600

    feat:router param

--- express/router/publicRouter.js
+++ express/router/publicRouter.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const publicRouter = express.Router();
 
+const log = (req, res, next) => {
+  console.log("I am Logging");
+  next();
+};
+
+publicRouter.all("*", log);
+
 publicRouter.get("/", (req, res) => {
   res.send("Homepage");
 });
