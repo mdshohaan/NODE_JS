6bb4b934a2f730d589204b3583f04384617bd805
mdshohaan <irfanulislamdu@gmail.com>
2025-04-02 21:34:03 +0600

    feat:All data error solved

--- express/error-hnadler/index.js
+++ express/error-hnadler/index.js
@@ -5,6 +5,15 @@ app.use((req, res, next) => {
   res.status(505).send("There was an Error");
 });
 
+app.get("/", [
+  function (req, res, next) {
+    fs.writeFile("/inaccessible-path", "data", next);
+  },
+  function (req, res) {
+    res.send("OK");
+  },
+]);
+
 app.get("/", (req, res) => {
   res.send(a);
 });
